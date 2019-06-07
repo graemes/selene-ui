0a2c2f0e1574e0aebcd31b74711dcda6bb6044fc
Chris Veilleux <veilleux.chris@gmail.com>
2019-06-07 15:00:25 -0500

    removed the now defunct maintenance page from routing modules

--- projects/market/src/app/app-routing.module.ts
+++ projects/market/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { MaintenancePageComponent } from 'shared';
 import { PageNotFoundComponent } from 'shared';
 
 const routes: Routes = [
     { path: '', redirectTo: '/skills', pathMatch: 'full' },
-    { path: 'maintenance', component: MaintenancePageComponent},
     { path: '**', component: PageNotFoundComponent }
 ];
 
--- projects/sso/src/app/app-routing.module.ts
+++ projects/sso/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { ChangePasswordComponent } from './modules/change-password/change-password.component';
 import { LoginComponent } from './modules/login/login.component';
 import { LogoutComponent } from './modules/logout/logout.component';
-import { MaintenancePageComponent } from 'shared';
 import { PageNotFoundComponent } from 'shared';
 import { NewAccountComponent } from './modules/new-account/new-account.component';
 
@@ -12,7 +11,6 @@ const routes: Routes = [
     { path: 'login',  component: LoginComponent },
     { path: 'logout', component: LogoutComponent },
     { path: 'change-password', component: ChangePasswordComponent},
-    { path: 'maintenance', component: MaintenancePageComponent},
     { path: 'new-account', component:  NewAccountComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
