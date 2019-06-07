c3ad63c2ca959bc4df3daea1fd78d831ce5515c8
Chris Veilleux <veilleux.chris@gmail.com>
2019-06-07 14:58:41 -0500

    removed maintenance page as it makes absolutely no sense to have that page within the application that is under maintenance.

--- projects/shared/src/projects.ts
+++ projects/shared/src/projects.ts
@@ -2,8 +2,6 @@
  * Public API Surface of shared
  */
 
-export * from './lib/shared.component';
 export * from './lib/snackbar/snackbar.component';
-export * from './lib/maintenance-page/maintenance-page.component';
 export * from './lib/page-not-found/page-not-found.component';
 export * from './lib/shared.module';
